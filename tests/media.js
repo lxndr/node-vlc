be0451bf8088de66342aab6c67b9afb1b92ca7df
lxndr <lxndr87i@gmail.com>
2018-03-21 10:28:18 +0300

    added tests for callbacks

--- tests/media.js
+++ tests/media.js
@@ -7,8 +7,19 @@ const fname = 'file://' + path.resolve('tests/fixtures/bird-whistling.wav');
 describe('VlcMedia', () => {
   let media = null;
 
-  it ('creates new', () => {
+  it('creates new', () => {
     media = new VlcMedia(fname);
+  });
+
+  it('sets a callback', () => {
+    media.onmeta = () => {};
+    media.onmeta = ()  => {}; // set twice
+    expect(typeof media.onmeta === 'function').to.be.true;
+  });
+
+  it('unsets a callback', () => {
+    media.onmeta = null;
+    expect(media.onmeta).to.be.undefined;
   });
 
   after(() => {
